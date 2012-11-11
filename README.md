5f3cfc99a45239e5204481368443c7cd1ee4c2d7
Felix Zapata <fzberlinches@gmail.com>
2012-11-11 19:35:31 +0100

    añadida versión sin jquery

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ariaLevelParser
 ===============
 
-A JavaScript parser to add the correct "aria-level" in all the headings tag on my page to prevent the bug with JAWS 12 and 13 in Internet Explorer
+A JavaScript parser to add the correct "aria-level" in all the headings tag on my page to prevent the bug with JAWS 12 and 13 in Internet Explorer based on "
+JAWS, IE and Headings in HTML5" http://www.accessibleculture.org/articles/2011/10/jaws-ie-and-headings-in-html5/
--- src/parser-level-heading.js
+++ src/parser-level-heading.js
@@ -1,28 +1,30 @@
 var parserLevelHeadings = function () {
 
 	function _parser(headings) {
-		headings.each(function(){
-			var $that = $(this),
-				$nodeName = $that.get(0).nodeName;
-				$aria_level = $nodeName.charAt(1);
-			$that.attr("role", "heading");
-			$that.attr("aria-level", $aria_level);
-		});
+		var len = headings.length,
+			i = 0;
+		for(; i < len; i++){
+			var headingTag = headings[i],
+				nodeName = headingTag.nodeName;
+				aria_level = nodeName.charAt(1);
+			headingTag.setAttribute("role", "heading");
+			headingTag.setAttribute("aria-level", aria_level);
+		}
 	}
 
 	function init () {
-		var $h1 = $("h1"),
-			$h2 = $("h2"),
-			$h3 = $("h3"),
-			$h4 = $("h4"),
-			$h5 = $("h5"),
-			$h6 = $("h6");
-			_parser($h1);
-			_parser($h2);
-			_parser($h3);
-			_parser($h4);
-			_parser($h5);
-			_parser($h6);
+		var h1 = document.getElementsByTagName("h1"),
+			h2 = document.getElementsByTagName("h2"),
+			h3 = document.getElementsByTagName("h3"),
+			h4 = document.getElementsByTagName("h4"),
+			h5 = document.getElementsByTagName("h5"),
+			h6 = document.getElementsByTagName("h6");
+			_parser(h1);
+			_parser(h2);
+			_parser(h3);
+			_parser(h4);
+			_parser(h5);
+			_parser(h6);
 	}
 
 	return {
